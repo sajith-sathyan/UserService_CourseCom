9487626db2b1ef2a117a4727d69d6b1a96dad26e
sajith-sathyan <113768183+sajith-sathyan@users.noreply.github.com>
2024-07-03 21:01:55 +0530

    Update server.js

--- frameworks/webserver/server.js
+++ frameworks/webserver/server.js
@@ -2,41 +2,41 @@ export default function serverConfig(app,mongoose,config){
 
 
 
-// Connect to MongoDB
-mongoose.connect(config.mongo.uri, { useNewUrlParser: true, useUnifiedTopology: true });
-12
-// Get the default connection
-const db = mongoose.connection;
-
-// Event handling for successful connection
-db.on('connected', () => {
-  console.log('Connected to MongoDB');
-});
-
-// Event handling for connection error
-db.on('error', (err) => {
-  console.error(`MongoDB connection error: ${err}`);
-});
-
-// Event handling for disconnection
-db.on('disconnected', () => {
-  console.log('Disconnected from MongoDB');
-});
-
-// Close the MongoDB connection when the Node process is terminated
-process.on('SIGINT', () => {
-  db.close(() => {
-    console.log('MongoDB connection closed through app termination');
-    process.exit(0);
+  // Connect to MongoDB
+  mongoose.connect('mongodb+srv://sajith:nnBzi1Pz39Mj69aF@cluster0.m8hdckt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+  12
+  // Get the default connection
+  const db = mongoose.connection;
+  
+  // Event handling for successful connection
+  db.on('connected', () => {
+    console.log('Connected to MongoDB');
   });
-});
-
-    function startServer(){
-        app.listen(config.port,()=>{
-            console.log(`UserAuth Server Start At port${config.port}`)
-        })
-    }
-    return{
-        startServer 
-    }
-}
+  
+  // Event handling for connection error
+  db.on('error', (err) => {
+    console.error(`MongoDB connection error: ${err}`);
+  });
+  
+  // Event handling for disconnection
+  db.on('disconnected', () => {
+    console.log('Disconnected from MongoDB');
+  });
+  
+  // Close the MongoDB connection when the Node process is terminated
+  process.on('SIGINT', () => {
+    db.close(() => {
+      console.log('MongoDB connection closed through app termination');
+      process.exit(0);
+    });
+  });
+  
+      function startServer(){
+          app.listen(config.port,()=>{
+              console.log(`UserAuth Server Start At port${config.port}`)
+          })
+      }
+      return{
+          startServer 
+      }
+  }
